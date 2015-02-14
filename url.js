cbfc0703f3c21b64c368bfcb63da5474ad672178
CômeB <come.burguburu@gmail.com>
2015-02-14 14:42:35 +0100

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,10 @@
 function hash() {
     var obj = {};
-    $(location.hash).slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {obj[tab[0]] = tab[1]; } });
+    $(location).prop("hash").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {obj[tab[0]] = tab[1]; } });
     return obj;
 }
 function param() {
     var obj = {};
-    $(location.search).slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {obj[tab[0]] = tab[1]; } });
+    $(location).prop("search").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {obj[tab[0]] = tab[1]; } });
     return obj;
 }
