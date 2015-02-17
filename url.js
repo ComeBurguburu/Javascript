7f1bcd976ead1045cc46eddd31769a469284db1e
CômeB <come.burguburu@gmail.com>
2015-02-17 10:55:45 +0100

    Update url.js
    functions always return array

--- url.js
+++ url.js
@@ -1,12 +1,10 @@
 function hash() {
     var obj = {};
-	$(location).prop("hash").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1]; } else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]]; obj[tab[0]].push(tab[1]); } } } });
-	return obj;
+    $(location).prop("hash").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1]; } else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]]; obj[tab[0]].push(tab[1]); } } } });
+    return obj;
 }
 function param() {
-    var obj = {};
-    function hash() {
-    var obj = {};
-	$(location).prop("search").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1]; } else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]]; obj[tab[0]].push(tab[1]); } } } });
-	return obj;
+   var obj = {};
+   $(location).prop("search").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = [tab[1]]; } else { obj[tab[0]].push(tab[1]); } } });
+   return obj;
 }
