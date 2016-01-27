08039d31d4153d187b8d9cf76e67002b08718223
CômeBurguburu <come.burguburu@gmail.com>
2016-01-27 16:24:11 +0100

    Update url.js
    Refactor code

--- url.js
+++ url.js
@@ -1,10 +1,15 @@
+"use strict";
+
 function hash() {
-    var obj = {};
-    $(location).prop("hash").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1]; } else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]]; obj[tab[0]].push(tab[1]); } } } });
-    return obj;
+	return extract("hash");
 }
+
 function param() {
-   var obj = {};
-   $(location).prop("search").slice(1).split("&").forEach(function (param) {var tab = param.split("="); if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = [tab[1]]; } else { obj[tab[0]].push(tab[1]); } } });
-   return obj;
+	return extract("search");
+}
+
+function extract(key) {
+	var obj = {};
+	$(location).prop(key).slice(1).split("&").forEach(function (param) {var tab = param.split("=");if (tab.length === 2) {if (obj[tab[0]] === undefined) {obj[tab[0]] = tab[1];} else {if (typeof obj[tab[0]] !== "object") {obj[tab[0]] = [obj[tab[0]]];obj[tab[0]].push(tab[1]);}}}});
+	return obj
 }
