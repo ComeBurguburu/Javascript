86821c5b53446f2b8f9d60e0f41352640fa80757
CômeBurguburu <come.burguburu@gmail.com>
2017-03-24 22:41:46 +0100

    Rewrote http_request
    now support all methods

--- http_request.js
+++ http_request.js
@@ -1,20 +1,31 @@
-function get(url,data,onSuccess,onError){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4){
-            if(xhttp.status == 200) {
-                try{
-                    var json=JSON.parse(xhttp.responseText);
-                    onSuccess(json);
-                }catch(e){
-                    onSuccess(xhttp.responseText);
-                }
+function http(method, url, data, onSuccess, onError) {
+	method = method.toUpperCase()
+	if (["POST", "GET", "PUT","DELETE","PATCH"].indexOf(method) == -1){ 
+		throw "method unknow"
+	}
 
-            }else{
-                onError(xhttp.status+" "+xhttp.statusText);
-            }
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send(JSON.stringify(data));
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (xhttp.readyState == 4) {
+			if (xhttp.status >= 200 && xhttp.status < 400) {
+				try {
+					var json = JSON.parse(xhttp.responseText);
+					onSuccess(json);
+				} catch (e) {
+					onSuccess(xhttp.responseText);
+				}
+
+			} else {
+				onError(xhttp.status, xhttp.statusText);
+			}
+		}
+	};
+	xhttp.open(method, url, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+	var urldata = Object.keys(data).map(
+		function (k) {
+			return k + "=" + encodeURI(data[k])
+		}).join("&")
+	xhttp.send(urldata);
+    return xhttp;
 }
